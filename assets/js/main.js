23c6c01d24290c7042b54d390dfdb6740db7b7cd
dayang4321 <dayang4321@users.noreply.github.com>
2020-11-17 11:34:16 +0100

    implement dorm api

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,4 +41,70 @@ $("#formCheck").click(function(){
         $('.itemsForSale').fadeIn(800);
     }
     
-    });
+});
+    
+
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var form = document.getElementById('signupForm');
+  // prevent submission
+    form.addEventListener('submit', function (event) {
+
+      const contactName = $('#contactName').val();
+      const contactState = $('#contactState').val();
+      const contactEmail = $('#contactEmail').val();
+      const contactPhone = $('#contactPhone').val();
+      const contactInterested = $('#contactInterested').val();
+      const contactSelling = $('#contactSelling').val();
+
+
+    console.log('submit')
+  if (form.checkValidity() === false) {
+  event.preventDefault();
+    event.stopPropagation();
+
+    // console.log('validated')    
+  } 
+  form.classList.add('was-validated');
+      // console.log('dated')
+      if (form.checkValidity() === true) { 
+        event.preventDefault();
+        event.stopPropagation();
+        $('.submit-btn').html('<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>Submitting...').addClass('disabled');
+        $.post("https://api.declutter.ng/website-form",
+        {
+           name : contactName? contactName : "" ,
+          state : contactState? contactState : "",
+          email : contactEmail? contactEmail : "" ,
+          phone : contactPhone? contactPhone : "",
+          items_interested : contactInterested? contactInterested : "",
+          items_to_sell : contactSelling? contactSelling : ""
+      },
+        function (data, status) {
+            if (data.success) {
+              $(".hero-title h2").fadeToggle(300);
+              $('.form-content').fadeOut(300);
+              $(".hero-title h2").html("We would be in touch with you very soon");
+              $(".hero-title").addClass('mx-auto');
+              $(".hero-title h2").fadeToggle(300);
+           
+          }
+            else {
+              $(".hero-title h2").fadeToggle(300);
+              $('.form-content').fadeOut(300);
+              $(".hero-title h2").html("Oops! Something went wrong.");
+              $(".hero-title").addClass('mx-auto');
+              $(".hero-title h2").fadeToggle(300);
+           
+          }
+          
+        });
+      }
+
+  }, false);
+
+  }, false);
+  })();
